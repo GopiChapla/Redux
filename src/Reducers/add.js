163200613d40f786fa8e327c01f9ad6b6d7deb8b
GopiChapla <104886956+GopiChapla@users.noreply.github.com>
2022-06-24 20:13:02 +0530

    redux

--- src/Reducers/add.js
+++ src/Reducers/add.js
@@ -1,12 +1,14 @@
 const initstate = ['hello']
 const add = (state = initstate, action) => {
-    switch (action.type) {
-        case "ADD":
-            return [...state, action.payload]
-        case "DELETE":
-            state.splice(action.payload,1)
-            return state
-        default: return state
+    if (action.type === "ADD") {
+        return [...state, action.payload]
+    }
+    else if (action.type === "DELETE") {
+        state.splice(action.payload, 1)
+        return state
+    }
+    else {
+        return state
     }
 }
 export default add
